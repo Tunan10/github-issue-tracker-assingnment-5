ddaf7aa067f07a5908c4f086970a6aa82e8c8b55
Tunan10 <tunansakib200@gmail.com>
2026-03-09 06:22:43 +0600

    adding toggle system

---
+++ script/dashboard.js
@@ -0,0 +1,16 @@
+function toggleStyle(id) {
+  const allbtn = document.getElementById('all-btn');
+  const openbtn = document.getElementById('open-btn');
+  const closedbtn = document.getElementById('closed-btn');
+
+  [allbtn, openbtn, closedbtn].forEach(btn => {
+    btn.classList.remove('bg-violet-600', 'text-white');
+    btn.classList.add('bg-white', 'text-gray-500');
+  });
+
+  const selected = document.getElementById(id);
+  selected.classList.remove('bg-white', 'text-gray-500');
+  selected.classList.add('bg-violet-700', 'text-white');
+}
+
+loadIssues();
